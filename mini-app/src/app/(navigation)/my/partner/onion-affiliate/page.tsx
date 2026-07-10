fb4fe81bc9ee9e40b56d915dc7a7a3f63e0c7fa3
Mahdi Farimani <m.farimany@gmail.com>
2026-07-10 14:57:07 +0300

    feat(mini-app): enforce guest locks on all user profile sub-routes

--- mini-app/src/app/(navigation)/my/partner/onion-affiliate/page.tsx
+++ mini-app/src/app/(navigation)/my/partner/onion-affiliate/page.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { ShareAndEarnPartnership } from "./ShareAndEarnPartnership";
+import { useUserStore } from "@/context/store/user.store";
+import LoginRequired from "@/app/_components/auth/LoginRequired";
 
 export default function FairlaunchAffiliatePage() {
+  const { user } = useUserStore();
+
+  if (!user) {
+    return <LoginRequired />;
+  }
+
   return (
     <>
       <ShareAndEarnPartnership />
     </>
   );
 }
+
